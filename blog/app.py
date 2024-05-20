86073ce1cd09958a20fa57acccad92588b84c01c
Zimin(Dennis) Yang <Decycle.yang@gmail.com>
2024-05-20 16:30:31 -0700

    changed blog theme

--- blog/app.py
+++ blog/app.py
@@ -2,13 +2,15 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 df = pd.read_csv('../models/yelp_restaurant_review_labelled.csv')
 
-categories = ["FOOD", "LOCATION", "ATOMSPHERE", "SERVICE", "PRICE", "MENU", "SPEED"]
+categories = ["FOOD", "LOCATION", "ATOMSPHERE",
+              "SERVICE", "PRICE", "MENU", "SPEED"]
 
 
-def draw_graph(threshold = 0.1):
+def draw_graph(threshold=0.1):
     random_entries = df.sample(1)
     for category in categories:
         usefulness = f'{category}_usefulness'
@@ -25,7 +27,8 @@ def draw_graph(threshold = 0.1):
                         dcc.Graph(
                             figure=px.bar(
                                 x=categories,
-                                y=[row[f'{category}_score'] for category in categories],
+                                y=[row[f'{category}_score']
+                                    for category in categories],
                                 title='Scores',
                                 labels={'x': 'Category', 'y': 'Score'},
                                 range_y=[1, 5]
@@ -38,21 +41,29 @@ def draw_graph(threshold = 0.1):
         for i, row in random_entries.iterrows()
     ]
 
-external_stylesheets = [dbc.themes.CERULEAN]
+
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+load_figure_template('sketchy')
+external_stylesheets = [dbc.themes.SKETCHY, dbc_css]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
 app.layout = dbc.Container(
     [
-        html.H1('Random Reviews'),
-        dbc.Button('New Review', id='refresh', n_clicks=0, style={'margin-bottom': '10px'}),
+        html.Title('Restaurant Reviews'),
+        html.H1('Decoders'),
+        html.P("""
+        With the rise of machine learning transformers such as large language models, we can utilize them to help us understand various aspects of the reviews.
+        """),
+        dbc.Button('New Review', id='refresh', n_clicks=0, class_name='mb-4'),
         dbc.Row(
             children=draw_graph(),
             id='bar-graphs'
         )
-    ]
+    ], class_name='w-50 dbc', fluid=True
 )
+
 
 @callback(
     Output('bar-graphs', 'children'),
